0697585eed666a28812b6c9ac3ac75ab587ec06f
Francisco <vieirafrancisco.dev@gmail.com>
2025-02-11 23:53:20 -0300

    Final changes to load_mysql_data endpoint

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mysql-http-gateway
 Serviço HTTP para acesso e gerenciamento de dados em um banco de dados MySQL privado hospedado no Render
+
+## Exemplo de uso:
+```bash
+curl -X POST -F "file=@/caminho/do/arquivo.txt" http://localhost:5000/load_mysql_data
+```
+
+## Em casos de erro:
+Ajustar .sql conforme sugerido na mensagem de erro
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
+from typing import Tuple
 
 from flask import Flask, request, jsonify
 import pymysql
-from environ import Env
+from environs import Env
 
 env = Env()
 
@@ -14,7 +15,13 @@ DB_NAME = env.str('DB_NAME')
 app = Flask(__name__)
 
 
-@app.route('/load_mysql_data')
+def handle_error(error: str, conn: pymysql.Connection) -> Tuple[dict, int]:
+    conn.rollback()
+    conn.close()
+    return {'error': error}, 500
+
+
+@app.route('/load_mysql_data', methods=['POST'])
 def load_mysql_data():
     if 'file' not in request.files:
         return 'Nenhum arquivo enviado', 400
@@ -35,11 +42,18 @@ def load_mysql_data():
         host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_NAME
     )
     with conn.cursor() as cursor:
+        conn.begin()
         for statement in sql_script.split(';'):
             statement = statement.strip()
             if statement:
-                cursor.execute(statement)
-
+                try:
+                    cursor.execute(statement)
+                except pymysql.err.ProgrammingError as pe:
+                    return handle_error(str(pe), conn)
+                except pymysql.err.InternalError as ie:
+                    return handle_error(str(ie), conn)
+                except Exception as e:
+                    return handle_error(str(e), conn)
     conn.commit()
     conn.close()
 
